6f17d037515e9bcf25e544ab4a5263a39a38bd39
stopbrother <stopbro0323@naver.com>
2024-09-06 17:18:34 +0900

    move: form,list, dashboard 컴포넌트 Home컴포넌트에 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import TodoContainer from "../components/home/TodoContainer";
+import styled from "styled-components";
+import TodoDashboard from "../components/home/TodoDashboard";
+import TodoForm from "../components/home/TodoForm";
+import TodoList from "../components/home/TodoList";
 
 const Home = () => {
-  return <TodoContainer></TodoContainer>;
+  return (
+    <TodoContainer>
+      <TodoDashboard />
+      <TodoForm />
+      <TodoList />
+    </TodoContainer>
+  );
 };
 
 export default Home;
+
+const TodoContainer = styled.div``;
