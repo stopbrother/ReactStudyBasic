e457947d1ad8cfdb4654db1060f74bfee78fc495
stopbrother <stopbro0323@naver.com>
2024-09-13 09:14:43 +0900

    update: context에서 api함수로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { TodoProvider } from "./context/TodoContext";
+
 import Router from "./shared/Router";
 
 const queryClient = new QueryClient();
--- src/api/TodoClient.js
+++ src/api/TodoClient.js
@@ -3,3 +3,28 @@ import axios from "axios";
 export const todoClient = axios.create({
   baseURL: "http://localhost:5000/todos",
 });
+
+export const getTodos = async () => {
+  const { data } = axios.todoClient.get("/");
+  return data;
+};
+
+export const getTodoDetail = async (id) => {
+  const { data } = axios.todoClient.get(`/${id}`);
+  return data;
+};
+
+export const postTodo = async (newTodoObj) => {
+  const { data } = axios.todoClient.post("/", newTodoObj);
+  return data;
+};
+
+export const toggleTodo = async (id, completed) => {
+  const { data } = axios.todoClient.patch(`/${id}`, { completed });
+  return data;
+};
+
+export const deleteTodo = async (id) => {
+  const { data } = axios.todoClient.delete(`/${id}`);
+  return data;
+};
