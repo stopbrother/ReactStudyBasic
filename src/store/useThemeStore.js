e90afe92be4e0b63e432f7e729e7f59def78b93f
stopbrother <stopbro0323@naver.com>
2024-09-23 17:04:28 +0900

    add: useThemeStore 생성

---
+++ src/store/useThemeStore.js
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+  darkMode: false,
+  toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
+}));
