5b0b21adaf70b2932427a5acdc19f900f11c4b79
stopbrother <stopbro0323@naver.com>
2024-09-23 17:32:43 +0900

    update: persist 사용하여 상태유지

--- src/store/useThemeStore.js
+++ src/store/useThemeStore.js
@@ -1,6 +1,14 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useThemeStore = create((set) => ({
-  darkMode: false,
-  toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
-}));
+export const useThemeStore = create(
+  persist(
+    (set) => ({
+      darkMode: false,
+      toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
+    }),
+    {
+      name: "theme-storage",
+    }
+  )
+);
